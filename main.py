49216ee9e2d03f4dd3d8a5c0cb694f04c240891f
Alexander Boulos <alexander.boulos10@gmail.com>
2024-09-20 17:38:45 -0400

    Added query endpoint and middleware

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from query import query_rag
+from pydantic import BaseModel
+import json
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # React app URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class QueryRequestString(BaseModel):
+    query_text: str
+
+@app.post("/query")
+def get_recipe(request:QueryRequestString):
+    query_text = request.query_text
+    if not query_text:
+        return {"error": "No query provided"}
+    string_response = query_rag(query_text)
+    json_response = json.loads(string_response)
+    print((json_response['title']))
+    return json_response
